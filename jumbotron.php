d8b26abe726c3d12006663f496502b63566afc82
LItSamm <112859236+LitSamm@users.noreply.github.com>
2023-12-06 16:39:58 +0700

    Add files via upload

---
+++ jumbotron.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Stylish Jumbotron</title>
+  <link rel="stylesheet" href="style_jumbotron.css">
+</head>
+
+<body>
+  <!-- Stylish Jumbotron -->
+  <section class="stylish-jumbotron">
+    <div class="container">
+      <div class="jumbo-content">
+        <img src="gambar/nikeair.jpg" alt="Trendy Fashion Image" class="trendy-image">
+        <div class="left-jb">
+          <h1 class="main-heading">Discover Your Style</h1>
+          <p class="sub-heading">Express yourself with our latest collection of trendy fashion.</p>
+          <a href="produk.php" class="btn-shop-now">Shop Now</a>
+        </div>
+      </div>
+    </div>
+  </section>
+</body>
+
+</html>
---
+++ operator.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Halaman Admin</title>
+    <style>
+        a,
+        button,
+        input[type=submit],
+        input[type=reset] {
+            font-family: Helvetica;
+            font-size: 15px;
+            background: #22a4cf;
+            color: white;
+            border: white 0px solid;
+            border-radius: 5px;
+            padding: 12px 20px;
+            margin-top: 10px;
+        }
+
+        h1 {
+            font-family: Helvetica;
+            font-size: 30px;
+            color: slateblue;
+        }
+    </style>
+</head>
+
+<body>
+    <center>
+    <h1>Halaman Admin</h1>
+    <button>
+        <a href="tambah_produk.php">Tambah Produk</a>
+    </button>
+    </center>
+</body>
+
+</html>
